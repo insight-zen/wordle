dc4a0045016db98568d1cee0b4f267412cfcee37
mu09_m1 <mu09@mbam1.local>
2022-02-28 18:08:12 -0500

    added ES6 module first draft

---
+++ bin/test.js
@@ -0,0 +1,4 @@
+
+// Dynamic Import
+const { default: wmd } = await import ("./bin/word_mod.mjs")
+wmd.info()
---
+++ bin/test.mjs
@@ -0,0 +1,5 @@
+
+// Module Import
+import WordList from "../lib/word_list.mjs"
+const wl = new WordList()
+wl.info()
